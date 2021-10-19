76736a3bb3c02c548c95a8e4e57b01ba7d2e560a
Yann Touil <yanntouil@gmail.com>
2021-10-19 16:11:56 +0200

    VerticalLayout test structure update

--- src/__tests__/VerticalLayout.js
+++ src/__tests__/VerticalLayout.js
@@ -4,16 +4,17 @@ import { localStorageMock } from "../__mocks__/localStorage.js"
 
 
 describe('Given I am connected as Employee', () => {
-  test("Then Icons should be rendered", () => {
-    Object.defineProperty(window, 'localStorage', { value: localStorageMock })
-    const user = JSON.stringify({
-      type: 'Employee'
+  describe('When I am on Bills page', () => {
+    test("Then Icons should be rendered in sidebar", () => {
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      const user = JSON.stringify({
+        type: 'Employee'
+      })
+      window.localStorage.setItem('user', user)
+      const html = VerticalLayout(120)
+      document.body.innerHTML = html
+      expect(screen.getByTestId('icon-window')).toBeTruthy()
+      expect(screen.getByTestId('icon-mail')).toBeTruthy()
     })
-    window.localStorage.setItem('user', user)
-    const html = VerticalLayout(120)
-    document.body.innerHTML = html
-    expect(screen.getByTestId('icon-window')).toBeTruthy()
-    expect(screen.getByTestId('icon-mail')).toBeTruthy()
   })
-
 })
